a809a01f6e4ca2fc8f9f368df69cab90fce7fd45
ady400 <raehan223@sma.belajar.id>
2025-04-17 10:44:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 gantengnyo")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "aplikasi ini di gunakan untuk menghitung ppm"
+
